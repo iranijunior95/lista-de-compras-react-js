2b1804bddddb8b4e9d035a05786069c6ba3193f2
Irani Junior <iranijunior95@gmail.com>
2025-01-10 21:42:01 -0300

    commit: componet InputDefault

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ButtonDefault from './components/ButtonDefault';
+import InputDefault from './components/InputDefault';
 import './App.css'
 
 function App() {
 
   return (
     <div className='container'>
+      <InputDefault />
       <ButtonDefault></ButtonDefault>
     </div>
   );
---
+++ src/components/InputDefault/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+function InputDefault() {
+    return (
+        <input 
+            type="text" 
+            name="input-default" 
+            id="input-default"
+            className='input-default'
+            placeholder='Adicione um novo item'
+        />
+    );
+}
+
+export default InputDefault;
